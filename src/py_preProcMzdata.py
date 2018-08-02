180123f590f6e18e58356d7aca1856e621e26958
mauraisa <aaron.maurais@bc.edu>
2018-08-02 10:55:21 -0400

    Initial commit.

---
+++ src/py_preProcMzdata.py
@@ -0,0 +1,13 @@
+
+import sys
+import argparse
+import pyopenms
+
+def main(argv):
+    parser = argparse.ArgumentParser(prog = 'py_preProcMzdata')
+
+    print('poop')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
